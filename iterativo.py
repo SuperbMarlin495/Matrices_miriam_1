3ab4457c44f0632ba6838e3fce1cd05a07dc6d54
Jim Loza <jimloza25@gmail.com>
2022-06-01 15:13:04 -0500

    Agregue la funcion multi

--- iterativo.py
+++ iterativo.py
@@ -2,22 +2,25 @@
 
 import numpy as np
 
-# A = [ [3,5], [6,8] ]
-# B = [ [9,2], [1, 4] ]
+# A = [ [2,0,1], [3,0,0], [5,1,1] ]
+# B = [ [1,0,1], [1,2,1], [1,1,0] ]
 
-
-A = [ [2,0,1], [3,0,0], [5,1,1] ]
-B = [ [1,0,1], [1,2,1], [1,1,0] ]
-
+A = [ [3,5], [6,8] ]
+B = [ [9,2], [1, 4] ]
 
 n = len(A)
-
 C = np.empty((n, n), int)
 
-for i in range(n):
-    for j in range(n):
-        C[i][j] = 0
-        for k in range(n):
-            C[i][j] = C[i][j] + A[i][k] * B[k][j]
 
-print(C)
+def multi_matrix(A, B):
+
+    for i in range(n):
+        for j in range(n):
+            C[i][j] = 0
+            for k in range(n):
+                C[i][j] = C[i][j] + A[i][k] * B[k][j]
+    
+    return C
+
+
+print(multi_matrix(A, B))
